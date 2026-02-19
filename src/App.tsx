4c3b8da7bccf4923498401f95aefce3c2e55039f
MantraCare AI <mantracare@example.com>
2026-02-19 13:00:13 +0530

    deploy: align with web.mantracare.com /app/ alias hosting

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/app">
         <Routes>
-          <Route path="/" element={<Index />} />
           <Route path="/mindfulness-benefits" element={<Index />} />
-          <Route path="/app/mindfulness-benefits" element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
